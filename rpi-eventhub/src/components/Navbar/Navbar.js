c4bca549634b49dcbf8848edd3f72a41b91aa08a
MeiH10 <huangm10@rpi.edu>
2024-02-02 17:51:25 -0500

    Created "components" folder and moved all components there. Added very simple nav bar component
    Created "components" folder and moved all components there. Added very simple nav bar component.

---
+++ rpi-eventhub/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+    return ( 
+        <nav class="crumbs">
+            <ol>
+                <li class="crumb"><a href="google.com">Bikes</a></li>
+                <li class="crumb"><a href="rpi.edu">BMX</a></li>
+                <li class="crumb">Jump Bike 3000</li>
+            </ol>
+        </nav>
+     );
+}
+ 
+export default Navbar;
