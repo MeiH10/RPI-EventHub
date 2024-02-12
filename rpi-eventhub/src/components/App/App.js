ac2b153c15188f4ba1f0f8b3ace8b0beb812e892
HenryTheall <thealh@rpi.edu>
2024-02-11 21:21:02 -0500

    Update App.js

--- rpi-eventhub/src/components/App/App.js
+++ rpi-eventhub/src/components/App/App.js
@@ -1,8 +1,23 @@
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from '../Navbar/Navbar';
 
+import logo from '../../assets/logo.svg';
+console.log(logo);
 
 function App() {
+
+  const [rotationAngle, setRotationAngle] = useState(0);
+
+  const handleRotateClick = () => {
+    // Update the rotation angle on click
+    setRotationAngle((prevAngle) => prevAngle + 100); // Rotate by  90 degrees
+  };
+
+  const logoStyle = {
+    transform: `rotate(${rotationAngle}deg)`,
+    transition: 'transform  1s ease-in-out', // Optional: smooth transition
+  };
 
   const title = "aaaaaaaaaakk World";
   return (
@@ -10,9 +25,17 @@ function App() {
 
       <Navbar></Navbar>
 
+      <a href="https://submitty.cs.rpi.edu/home" target="_blank">
+        <img id="Logo" src={logo} style={logoStyle} width={250} height={250} alt="Logo" />
+      </a>
+
+      <h1>
+        <button id="rotateButton" onClick={handleRotateClick}>Rotate Image</button>      
+      </h1>
+
       <div className="content">
         <h1>{title}</h1>
-      </div>
+      </div>      
     </div>
   )
 }
