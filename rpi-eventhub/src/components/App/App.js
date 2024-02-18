07ffbacde1019da26104d0d9f2de1d88fbdb4847
jordyny <jordyn.young2234@gmail.com>
2024-02-17 23:24:36 -0500

    added copyright into footer

--- rpi-eventhub/src/components/App/App.js
+++ rpi-eventhub/src/components/App/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import Navbar from '../Navbar/Navbar';
-
+import Footer from '../Footer/Footer'; 
 
 function App() {
 
   const title = "aaaaaaaaaakk World";
   return (
+    <>
     <div className="App">
 
-      <Navbar></Navbar>
+      <Navbar />
 
       <div className="content">
         <h1>{title}</h1>
       </div>
     </div>
+    <Footer />
+    </>
   )
 }
 
