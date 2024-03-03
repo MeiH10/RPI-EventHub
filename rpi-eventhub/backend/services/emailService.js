6db8fb2b6f908c7f42cb1494469eb707adbae1d7
MeiH10 <huangm10@rpi.edu>
2024-03-02 23:14:11 -0500

    completed email verification system using sendgrid

--- rpi-eventhub/backend/services/emailService.js
+++ rpi-eventhub/backend/services/emailService.js
@@ -1,32 +1,30 @@
 // backend/services/emailService.js
 const nodemailer = require('nodemailer');
-require('dotenv').config({ path: '../.env' });
+const sgTransport = require('nodemailer-sendgrid');
+
+const path = require('path');
+
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 
+const mailer = nodemailer.createTransport(sgTransport({
+  apiKey: process.env.SENDGRID_API_KEY
+}));
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
 
-const sendEmail = async ({ to, subject, text, html }) => {
+const sendEmail = async ({ to, subject, text }) => {
   const mailOptions = {
-    from: process.env.EMAIL_USERNAME,
+    from: process.env.EMAIL_FROM, // Your email address or one authorized by SendGrid
     to,
     subject,
     text,
-    html,
   };
 
   try {
-    await transporter.sendMail(mailOptions);
-    console.log(`Email sent successfully to ${to}`);
+    await mailer.sendMail(mailOptions);
+    console.log('Email sent successfully');
   } catch (error) {
-    console.error(`Error sending email to ${to}: ${error}`);
-    throw error;
+    console.error('Failed to send email', error);
   }
 };
 
