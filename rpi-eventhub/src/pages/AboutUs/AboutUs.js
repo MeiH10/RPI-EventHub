373025dd64460932869ee84f0393682b8c2d3e44
HenryTheall <thealh@rpi.edu>
2024-03-12 17:24:03 -0400

    About Us Page Graphics
    I split the about us page into a left and right side, the left side containing text and the right side containing an image. I also added a scrollbar but it isn't working as intended right now.

--- rpi-eventhub/src/pages/AboutUs/AboutUs.js
+++ rpi-eventhub/src/pages/AboutUs/AboutUs.js
@@ -2,12 +2,43 @@ import './AboutUs.css'
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
+import logo from '../../assets/logo.svg';
+console.log(logo);
 
 function AboutUs() {
     return (
+
         <div className="AboutUs">
             <Navbar/>
-            <div class="about">About Us Page</div>
+
+            <div className="split left">
+                <div className="centered">
+                    <h1>Mission Statement</h1>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                    mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+
+            <div className="split right">
+                <div className="centered">
+                    <img src={logo} alt="Logo"></img>
+                </div>
+            </div>
+
+            <div className="modal-container">
+
+                <div className="modal-content">
+                    <h1>adsadsa</h1>
+                </div>
+
+            </div>
+
             <Footer/>
         </div>
     )
