657b02966628255a15c83041852a818945efc05c
MeiH10 <huangm10@rpi.edu>
2024-03-19 20:18:10 -0400

    big structural changes. moved new files into updated structure

--- rpi-eventhub/frontend/src/index.js
+++ rpi-eventhub/frontend/src/index.js
@@ -3,15 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import AboutUs from './pages/AboutUs/AboutUs';
+import Events from './pages/Events/Events';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "events",
+    element: <Events/>,
+  },
+  {
+    path: "about-us",
+    element: <AboutUs/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> 
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
