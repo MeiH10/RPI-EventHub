b45ce7b99597b211b0af6951c98f20d14d7f19cc
HenryTheall <thealh@rpi.edu>
2024-03-22 16:21:08 -0400

    About Us Page Layout
    Start of the about us page including the "about us" header and mission statement filler text. It also includes a large image that can be replaced and a line page break.

--- rpi-eventhub/src/pages/AboutUs/AboutUs.js
+++ rpi-eventhub/src/pages/AboutUs/AboutUs.js
@@ -2,15 +2,63 @@ import './AboutUs.css'
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
+import EventHubLogo from "../../assets/EventHubLogo1.svg";
+import RPIseal from "../../assets/RF0010-04 Small Seal-RGB-White.svg";
 
 function AboutUs() {
     return (
-        <div className="AboutUs">
-            <Navbar/>
-            <div class="about">About Us Page</div>
-            <Footer/>
+        <div>
+          <Navbar />
+    
+          <div className="container-fluid content">
+            
+            <div className="row">
+              <div className="col-7 p-5">
+    
+                <div className="title text-start">
+                  <h1>About Us</h1>
+                  <h6>RPI EventHub</h6>
+                </div>
+                
+    
+                <div className="grid">
+    
+                    <div className="about-us-text">
+                        <h4>Mission Statement</h4>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                    
+                    
+          
+                </div>
+                 
+                
+
+            </div>
+    
+              <div className="col-5 ">
+                <img src={EventHubLogo} alt="logo" width="500" height="400"></img>
+              </div>
+
+            </div>
+
+            <hr></hr>
+
+            <div class="developers">
+                <h4>Developers</h4>
+                
+            </div>
+
+          </div>
+          <Footer />
         </div>
-    )
+      );
 }
 
 export default AboutUs
