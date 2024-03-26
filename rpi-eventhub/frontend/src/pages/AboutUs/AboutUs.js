c20fea0e7283f983d091ab3a8409c7ae4c78ae66
HenryTheall <thealh@rpi.edu>
2024-03-26 17:07:45 -0400

    About Us Page Complete
    I've replaced all of the template text and images with accurate information, including adding everyone's contact information (emails).

--- rpi-eventhub/frontend/src/pages/AboutUs/AboutUs.js
+++ rpi-eventhub/frontend/src/pages/AboutUs/AboutUs.js
@@ -3,7 +3,9 @@ import AbousUsCSS from './AboutUs.modular.css'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import EventHubLogo from "../../assets/EventHubLogo1.svg";
-import RPIseal from "../../assets/RF0010-04 Small Seal-RGB-White.svg";
+import RPIseal from '../../assets/RF0010-04 Small Seal-RGB-White.svg';
+import RPIBridgePhoto from '../../assets/RPIBridgePhoto.jpg'
+import ProfilePicImage from '../../assets/ProfilePicImage.svg'
 
 function AboutUs() {
     return (
@@ -25,25 +27,20 @@ function AboutUs() {
     
                     <div className="about-us-text">
                         <h4>Mission Statement</h4>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        EventHub is dedicated to connecting the students of RPI with events
+                        happening all over campus. Through this website, we hope to foster
+                        greater community, connection, and collaboration throughout the campus.
+                        Our hope is for RPI students and staff to be able to effortlessly create,
+                        advertise, and explore diverse campus events, fostering a vibrant and
+                        connected university community.
                     </div>
                     
-                    
-          
                 </div>
                  
-                
-
             </div>
     
-              <div className="col-5 ">
-                <img src={EventHubLogo} alt="logo" width="500" height="400"></img>
+              <div className="col-5 p-5">
+                <img src={RPIBridgePhoto} alt="logo" width="480"></img>
               </div>
 
             </div>
@@ -55,60 +52,62 @@ function AboutUs() {
                 <div className="row">
 
                   <div className="column">
-                    <img src={RPIseal} height="150"></img>
-                    <h6 className="devtext">
-                      <a href="https://www.google.com/" /* <- placeholder for email */ target="_blank">First Last</a>
-                    </h6>
-                    <p className="devtext">Position</p>
+                    <img src={ProfilePicImage} height="150"></img>
+                    <h6 className="devtext">Mei H - Project Lead</h6>
+                    <p className="devtext">
+                      <a href="mailto:huangm10@rpi.edu" target="_blank">huangm10@rpi.edu</a>
+                    </p>
                   </div>
 
                   <div className="column">
-                    <img src={RPIseal} height="150"></img>
-                    <h6 className="devtext">
-                      <a href="https://www.google.com/" /* <- placeholder for email */ target="_blank">First Last</a>
-                    </h6>
-                    <p className="devtext">Position</p>
+                    <img src={ProfilePicImage} height="150"></img>
+                    <h6 className="devtext">William F</h6>
+                    <p className="devtext">
+                      <a href="mailto:fernaw@rpi.edu" target="_blank">fernaw@rpi.edu</a>
+                    </p>
                   </div>
 
                   <div className="column">
-                    <img src={RPIseal} height="150"></img>
-                    <h6 className="devtext">
-                      <a href="https://www.google.com/" /* <- placeholder for email */ target="_blank">First Last</a>
-                    </h6>
-                    <p className="devtext">Position</p>
+                    <img src={ProfilePicImage} height="150"></img>
+                    <h6 className="devtext">Nithin V</h6>
+                    <p className="devtext">
+                      <a href="mailto:vadakn@rpi.edu" target="_blank">vadakn@rpi.edu</a>
+                    </p>
                   </div>
 
                   <div className="column">
-                    <img src={RPIseal} height="150"></img>
-                    <h6 className="devtext">
-                      <a href="https://www.google.com/" /* <- placeholder for email */ target="_blank">First Last</a>
-                    </h6>
-                    <p className="devtext">Position</p>
+                    <img src={ProfilePicImage} height="150"></img>
+                    <h6 className="devtext">Jordyn Y</h6>
+                    <p className="devtext">
+                      <a href="mailto:youngj22@rpi.edu" target="_blank">youngj22@rpi.edu</a>
+                    </p>
                   </div>
 
                   <div className="column">
-                    <img src={RPIseal} height="150"></img>
-                    <h6 className="devtext">
-                      <a href="https://www.google.com/" /* <- placeholder for email */ target="_blank">First Last</a>
-                    </h6>
-                    <p className="devtext">Position</p>
+                    <img src={ProfilePicImage} height="150"></img>
+                    <h6 className="devtext">Henry T</h6>
+                    <p className="devtext">
+                      <a href="mailto:thealh@rpi.edu" target="_blank">thealh@rpi.edu</a>
+                    </p>
                   </div>
 
                   <div className="column">
-                    <img src={RPIseal} height="150"></img>
-                    <h6 className="devtext">
-                      <a href="https://www.google.com/" /* <- placeholder for email */ target="_blank">First Last</a>
-                    </h6>
-                    <p className="devtext">Position</p>
+                    <img src={ProfilePicImage} height="150"></img>
+                    <h6 className="devtext">Hari K</h6>
+                    <p className="devtext">
+                      <a href="mailto:kimh21@rpi.edu" target="_blank">kimh21@rpi.edu</a>
+                    </p>
                   </div>
 
                   <div className="column">
-                    <img src={RPIseal} height="150"></img>
-                    <h6 className="devtext">
-                      <a href="https://www.google.com/" /* <- placeholder for email */ target="_blank">First Last</a>
-                    </h6>
-                    <p className="devtext">Position</p>
+                    <img src={ProfilePicImage} height="150"></img>
+                    <h6 className="devtext">Felix T</h6>
+                    <p className="devtext">
+                      <a href="mailto:tianf2@rpi.edu" target="_blank">tianf2@rpi.edu</a>
+                    </p>
                   </div>
+
+                  
 
                 </div>
 
