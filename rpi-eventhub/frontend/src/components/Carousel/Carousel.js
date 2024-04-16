3761e65f611d9e7f8adeb6ef752d70412a9a1fd2
jordyny <jordyn.young2234@gmail.com>
2024-04-16 16:49:07 -0400

    installing bootstrap and basics of caption

--- rpi-eventhub/frontend/src/components/Carousel/Carousel.js
+++ rpi-eventhub/frontend/src/components/Carousel/Carousel.js
@@ -1,46 +1,49 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './Carousel.css'; // Ensure this CSS file contains your styles
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
 import poster1 from './poster1.jpeg';
 import poster2 from './poster2.jpeg';
 import poster3 from './poster3.png';
 import poster4 from './poster4.jpeg';
 import poster5 from './poster5.png';
 
-const images = [poster1, poster2, poster3, poster4, poster5];
+const images = [
+  { src: poster1, caption: "Caption for Poster 1" },
+  { src: poster2, caption: "Caption for Poster 2" },
+  { src: poster3, caption: "Caption for Poster 3" },
+  { src: poster4, caption: "Caption for Poster 4" },
+  { src: poster5, caption: "Caption for Poster 5" }
+];
 
 const ImageCarousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const intervalRef = useRef(null); // Use useRef to hold the interval ID
+  const intervalRef = useRef(null);
 
-  // Function to go to the next image
   const goToNext = () => {
     setActiveIndex((current) => (current === images.length - 1 ? 0 : current + 1));
   };
 
-  // Function to go to the previous image
   const goToPrev = () => {
     setActiveIndex((current) => (current === 0 ? images.length - 1 : current - 1));
   };
 
-  // Function to reset and start the timer
   const resetTimer = () => {
-    clearInterval(intervalRef.current); // Clear the existing timer
-    intervalRef.current = setInterval(goToNext, 3000); // Start a new timer
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(goToNext, 3000);
   };
 
   useEffect(() => {
-    resetTimer(); // Start the timer when the component mounts
-
-    // Clear the timer when the component unmounts
+    resetTimer();
     return () => clearInterval(intervalRef.current);
-  }, []); // Empty dependency array means this effect runs only once on mount
+  }, []);
 
   return (
     <div className="carousel">
       <div className="main-image">
-        {/* Image Container */}
-        <img src={images[activeIndex]} alt={`Slide ${activeIndex}`} />
-        {/* Navigation Buttons */}
+        <img src={images[activeIndex].src} alt={`Slide ${activeIndex}`} className="img-fluid" />
+        <div className="carousel-caption d-none d-md-block">
+          <h5>{images[activeIndex].caption}</h5>
+        </div>
         <button onClick={() => { goToPrev(); resetTimer(); }} className="prev-button">←</button>
         <button onClick={() => { goToNext(); resetTimer(); }} className="next-button">→</button>
       </div>
