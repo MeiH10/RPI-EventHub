f94dd5d823083c817127c1f85a483150292ce161
FlyM1ss <tianf2@rpi.edu>
2024-04-16 17:29:19 -0400

    Fixed poster dynamic scaling
    CSS logic + data structure change

--- rpi-eventhub/frontend/src/components/EventPosterOnly/EventPoster.js
+++ rpi-eventhub/frontend/src/components/EventPosterOnly/EventPoster.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './EventPoster.css';
 
-const EventPoster = ({ title, posterSrc, description, width, height }) => {
+const EventPoster = ({ title, posterSrc, description }) => {
     return (
-        <div className="event-poster-container" style={{ width: width, height: height }}>
-            <img src={posterSrc} alt={title} className="event-poster-img" style={{ width: '100%', height: '100%' }}/>
+        <div className="event-poster-container">
+            <img src={posterSrc} alt={title} className="event-poster-img"/>
             <div className="event-poster-details">
                 <h2 className="event-poster-title">{title}</h2>
                 <p className="event-poster-description">{description}</p>
