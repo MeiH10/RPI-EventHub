1b1151cd9c73d93627ddf5d9c3d3fd9405229f03
williamryanfernando <156137397+williamryanfernando@users.noreply.github.com>
2024-04-24 23:57:42 -0400

    Comments for Confetti
    Documentation for Confetti and comments to make the code a little bit more readable.

--- rpi-eventhub/frontend/src/components/App/Confetti/Confetti.js
+++ rpi-eventhub/frontend/src/components/App/Confetti/Confetti.js
@@ -1,3 +1,6 @@
+//uses random movement and image color in order to create confetti and have it fall down. 
+//Searched up example code but as an alternative Submitty's confetti code could also be used. 
+
 import React, { useState, useEffect } from 'react';
 import './Confetti.modular.css';
 
@@ -6,6 +9,7 @@ const Confetti = () => {
 
   useEffect(() => {
     const animationFrame = () => {
+    // Create new confetti particle
       const newConfetti = {
         left: `${Math.random() * 100}vw`, // Random hoz position
         animationDuration: `${Math.random() * 2 + 1}s`, // Random animation duration
@@ -13,6 +17,7 @@ const Confetti = () => {
         opacity: Math.random() * 0.8 + 0.2, // Random opacity
       };
 
+      //add to array
       setConfetti((prevConfetti) => [...prevConfetti, newConfetti]);
 
       setTimeout(() => {
@@ -20,12 +25,14 @@ const Confetti = () => {
       }, 2000);
     };
 
+    // Create confetti at regular intervals
     const intervalId = setInterval(animationFrame, 200);
 
     return () => clearInterval(intervalId);
   }, []);
 
   return (
+     // Render confetti particles
     <div className="confetti-container">
       {confetti.map((confettiPiece, index) => (
         <div
--- rpi-eventhub/frontend/src/components/App/Confetti/ConfettiButton.js
+++ rpi-eventhub/frontend/src/components/App/Confetti/ConfettiButton.js
@@ -1,11 +1,13 @@
-//Packages the previous confetti component into 
+//Packages the previous confetti component into a button so that it can be applied to some of the buttons
+//can be attached to stuff like the RSVP button
 
 import React, { useState } from 'react';
 import Confetti from './Confetti.js';
 
 const ConfettiButton = ({ children }) => {
   const [showConfetti, setShowConfetti] = useState(false);
-
+  
+  // State to track whether to show confetti
   const handleClick = () => {
     setShowConfetti(true);
     setTimeout(() => setShowConfetti(false), 2000); // Hide confetti after 2 seconds
