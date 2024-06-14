7369329a6a4fb38b79e460cf2617ae905ffb1d0b
jiec137 <revinchen21@gmail.com>
2024-06-14 19:50:25 -0400

    splited the caption to make it less bulky
    The caption above now only displays the event name. The location and date are located below the poster. Eliminated the miliseconds and added day of the week to date.

--- rpi-eventhub/frontend/src/components/Carousel/Carousel.js
+++ rpi-eventhub/frontend/src/components/Carousel/Carousel.js
@@ -3,7 +3,23 @@ import './Carousel.css'; // Ensure this CSS file contains your styles
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import axios from 'axios';
 
+
+
 const placeholderImage = 'https://via.placeholder.com/518x671?text=No+Image+Available'; // URL to a default placeholder image
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const options = { 
+    month: '2-digit', 
+    day: '2-digit', 
+    year: 'numeric', 
+    weekday: 'short', 
+    hour: 'numeric', 
+    minute: '2-digit', 
+    hour12: true 
+  };
+  return date.toLocaleDateString('en-US', options).replace(',', '').replace(',', ' @');
+};
 
 const ImageCarousel = () => {
   const [events, setEvents] = useState([]);
@@ -18,7 +34,8 @@ const ImageCarousel = () => {
           src: event.image || placeholderImage, // Use the event image or the placeholder if no image exists
           caption: event.title,
           location: event.location,
-          date: new Date(event.date).toLocaleString() // Format date as needed
+          date: formatDate(event.date),// Format date as needed
+          
         })));
       } catch (error) {
         console.error('Failed to fetch events:', error);
@@ -46,13 +63,18 @@ const ImageCarousel = () => {
     return () => clearInterval(intervalRef.current);
   }, [events]);
 
+
+
   return (
     <div className="carousel">
       <div className="main-image">
         {events.length > 0 && (
           <>
-            <div className="caption-above">{`${events[activeIndex].caption} - ${events[activeIndex].location} - ${events[activeIndex].date}`}</div>
+            <div className="caption-above">{`${events[activeIndex].caption.toUpperCase()}`}</div>
             <img src={events[activeIndex].src} alt={`Slide ${activeIndex}`} />
+            <div className="caption-below">
+              {`${events[activeIndex].location.toUpperCase()}  - ${events[activeIndex].date}`}
+            </div>
             <button onClick={() => { goToPrev(); resetTimer(); }} className="prev-button">
               <i className="bi bi-chevron-left"></i>
             </button>
