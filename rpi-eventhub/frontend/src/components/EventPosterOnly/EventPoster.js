67e28ce85a4cf03958b135b277146720c158657b
MeiH10 <huangm10@rpi.edu>
2024-07-04 07:42:32 -0400

    made delete button only visible to admin and author of the post

--- rpi-eventhub/frontend/src/components/EventPosterOnly/EventPoster.js
+++ rpi-eventhub/frontend/src/components/EventPosterOnly/EventPoster.js
@@ -1,8 +1,10 @@
 import React, { useCallback } from 'react';
 import style from './EventPoster.module.css';
 import { useEvents } from '../../context/EventsContext';
+import { useAuth } from '../../context/AuthContext';
 
 const EventPoster = ({ id, title, posterSrc, description, author }) => {
+  const { username } = useAuth();  // Destructure username from useAuth
   const { deleteEvent } = useEvents();
 
   const handleDelete = useCallback(async () => {
@@ -13,16 +15,25 @@ const EventPoster = ({ id, title, posterSrc, description, author }) => {
     }
   }, [id, deleteEvent]);
 
+  const canSeeDeleteButton = (user_name) => {
+    console.log('user_name: ', user_name);
+    console.log('author: ', author);
+
+    if (user_name === 'admin' || user_name === author) {
+      return true;
+    }
+    return false;
+  }
+
   return (
     <div className={style.eventPosterContainer}>
       <img src={posterSrc} alt={title} className={style.eventPosterImg} />
       <div className={style.eventPosterDetails}>
         <h2 className={style.eventPosterTitle}>{title}</h2>
         <p className={style.eventPosterDescription}>{description}</p>
-        <button onClick={handleDelete} className="delete-button btn-danger btn">Delete</button>
+        {canSeeDeleteButton(username) && <button onClick={handleDelete} className="delete-button btn-danger btn">Delete</button>}
         <p>Posted by {author}</p>
       </div>
-
     </div>
   );
 };
