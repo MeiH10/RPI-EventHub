c0b0598d95902f9931e32c6cf93b3c6657328c3c
MeiH10 <huangm10@rpi.edu>
2024-07-09 22:24:58 -0400

    compressed images/flyers before hosting them on imgbb

--- rpi-eventhub/frontend/src/components/EventPosterOnly/EventPoster.js
+++ rpi-eventhub/frontend/src/components/EventPosterOnly/EventPoster.js
@@ -2,11 +2,13 @@ import React, { useCallback } from 'react';
 import style from './EventPoster.module.css';
 import { useEvents } from '../../context/EventsContext';
 import { useAuth } from '../../context/AuthContext';
+import { useEffect, useState } from 'react';
 
 const EventPoster = ({ id, title, posterSrc, description, author, tags }) => {
   console.log("tags: ", tags);
   const { username } = useAuth();  // Destructure username from useAuth
   const { deleteEvent } = useEvents();
+  const [imageSize, setImageSize] = useState(null);
 
   const handleDelete = useCallback(async () => {
     try {
@@ -26,6 +28,24 @@ const EventPoster = ({ id, title, posterSrc, description, author, tags }) => {
     return false;
   }
 
+
+  useEffect(() => {
+    const fetchImageSize = async () => {
+      try {
+        const response = await fetch(posterSrc, { method: 'HEAD' });
+        const contentLength = response.headers.get('Content-Length');
+        if (contentLength) {
+          setImageSize((parseInt(contentLength) / 1024).toFixed(2)); 
+        }
+      } catch (error) {
+        console.error('Failed to fetch image size:', error);
+      }
+    };
+
+    fetchImageSize();
+  }, [posterSrc]);
+
+
   return (
     <div className={style.eventPosterContainer}>
       <img src={posterSrc} alt={title} className={style.eventPosterImg} />
@@ -34,6 +54,7 @@ const EventPoster = ({ id, title, posterSrc, description, author, tags }) => {
         <p className={style.eventPosterDescription}>{description}</p>
         {canSeeDeleteButton(username) && <button onClick={handleDelete} className="delete-button btn-danger btn">Delete</button>}
         <p>Posted by {author}</p>
+        {/* {imageSize && <p>Image Size: {imageSize} KB</p>} */}
         <ul>
           {tags.map((tag, index) =>(
           <li key={index}>{tag}</li>
