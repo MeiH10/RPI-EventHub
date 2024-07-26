895667a7ab54a84de0ae053ee03534a9d18e65ed
pashok4865 <136192235+pashok4865@users.noreply.github.com>
2024-07-26 14:53:17 -0400

    Made Changes to Files to Improve Formatting
    Went through each of the files and made improvements to formatting by ensuring code was on appropriate indentation level, etc.

--- rpi-eventhub/frontend/src/components/SearchBar/SearchBar.js
+++ rpi-eventhub/frontend/src/components/SearchBar/SearchBar.js
@@ -1,55 +1,58 @@
-// src/components/SearchBar/SearchBar.jsx
-
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SearchBarCSS from './SearchBar.module.css'; // Adjust the import path if necessary
 import { useEvents } from '../../context/EventsContext'; // Adjust the import path if necessary
 
 const SearchBar = () => {
-    const navigate = useNavigate();
-    const { events, fetchEvents } = useEvents();
-    const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const { events, fetchEvents } = useEvents();
+  const [searchTerm, setSearchTerm] = useState('');
 
-    useEffect(() => {
-        fetchEvents();
-    }, [fetchEvents]);
+  useEffect(() => {
+    fetchEvents();
+  }, [fetchEvents]);
 
-    const handleSearchClick = () => {
-        const searchWords = searchTerm.toLowerCase().split(' ');
+  const handleSearchClick = () => {
+    const searchWords = searchTerm.toLowerCase().split(' ');
 
-        const filtered = events.filter(event => {
-            const eventWords = event.title.toLowerCase().split(' ');
-            const eventTags = event.tags.map(tag => tag.toLowerCase());
-            return searchWords.some(word => eventWords.includes(word) || eventTags.includes(word));
-        });
+    const filtered = events.filter(event => {
+      const eventWords = event.title.toLowerCase().split(' ');
+      const eventTags = event.tags.map(tag => tag.toLowerCase());
+      return searchWords.some(word => eventWords.includes(word) || eventTags.includes(word));
+    });
 
-        const sorted = filtered.sort((a, b) => new Date(b.date) - new Date(a.date));
+    const sorted = filtered.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-        navigate('/search', { state: { results: sorted } });
-    };
+    navigate('/search', { state: { results: sorted } });
+  };
 
-    const handleSearchInputChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            handleSearchClick();
-        }
-    };
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearchClick();
+    }
+  };
 
-    return (
-        <div className={SearchBarCSS.searchBarContainer}>
-            <input
-                className={SearchBarCSS.searchInput}
-                type="text"
-                placeholder="Search for an event!"
-                onChange={handleSearchInputChange}
-                onKeyPress={handleKeyPress}
-            />
-            <button className={SearchBarCSS.searchButton} onClick={handleSearchClick}>Search</button>
-        </div>
-    );
+  return (
+    <div className={SearchBarCSS.searchBarContainer}>
+      <input
+        className={SearchBarCSS.searchInput}
+        type="text"
+        placeholder="Search for an event!"
+        onChange={handleSearchInputChange}
+        onKeyPress={handleKeyPress}
+      />
+      <button
+        className={SearchBarCSS.searchButton}
+        onClick={handleSearchClick}
+      >
+        Search
+      </button>
+    </div>
+  );
 };
 
 export default SearchBar;
--- rpi-eventhub/frontend/src/pages/Playground/Playground.js
+++ rpi-eventhub/frontend/src/pages/Playground/Playground.js
@@ -1,48 +1,42 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import RsvpButton from '../../components/rsvp-button/RsvpButton';
-import logo from "./logo.svg";
-import logo2 from "./nyc.jpg"
-import React from 'react';
-import { motion } from 'framer-motion';   
 import HoverImagePopup from '../../components/ImagePopUp/ImagePopUp';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './Playground.module.css'
-// import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
-import Navbar from '../../components/Navbar/Navbar';
-
+import logo from "./logo.svg";
+import logo2 from "./nyc.jpg";
+import styles from './Playground.module.css';
 
 const Playground = () => {
-    const title = "Hello World";
-    return (
-  
-      <div className={styles.App}>
-          <Navbar />
-        <div className={`${styles.content} containerFluid`}>
-          <h1>{title}</h1>
-        </div>
-        <motion.img
-          
-          src = {logo}
-          alt="IMAGE HERE"  
-          style = {{width: '400px', height: '300px'}}
-          animate={{ y: [10, 30, 10], x: [0,0, 0]}}
-          transition={{
-            duration:  5,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-          
-        />
-        <div>
-          <HoverImagePopup src= {logo2} alt="Insert image here"/>
-  
-        </div>
-        <Footer />
-        <RsvpButton />
+  const title = "Hello World";
+
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <div className={`${styles.content} containerFluid`}>
+        <h1>{title}</h1>
       </div>
-    )
+      <motion.img
+        src={logo}
+        alt="IMAGE HERE"
+        style={{ width: '400px', height: '300px' }}
+        animate={{ y: [10, 30, 10], x: [0, 0, 0] }}
+        transition={{
+        duration: 5,
+        repeat: Infinity,
+        ease: "easeInOut",
+        }}
+      />
+      <div>
+        <HoverImagePopup src={logo2} alt="Insert image here" />
+      </div>
+      <Footer />
+      <RsvpButton />
+    </div>
+  );
+};
 
-
-}
- 
 export default Playground;
