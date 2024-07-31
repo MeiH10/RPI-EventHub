b9defc304820602f087a62a5926cc710f803200c
jiec137 <revinchen21@gmail.com>
2024-07-31 15:47:53 -0400

    Create ThemeContext.js

---
+++ rpi-eventhub/frontend/src/context/ThemeContext.js
@@ -0,0 +1,24 @@
+// src/context/ThemeContext.js
+import React, { createContext, useState, useEffect } from 'react';
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeContext, ThemeProvider };
