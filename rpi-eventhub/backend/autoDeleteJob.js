677080b49d13879ac380c56e6f0a2e6083a4a3c9
Nithin S Vadakken <nithinvadakken@gmail.com>
2024-10-15 16:20:44 -0400

    autoDelete
    deleting old events from the d

---
+++ rpi-eventhub/backend/autoDeleteJob.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const cron = require('node-cron');
+const Event = require('./path/to/event');
+
+mongoose.connect(process.env.MONGO_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+})
+   .then(() => console.log('MongoDB connected using env file'))
+   .catch((err) => console.log(err));
+
+const deleteOldEvents = async () => {
+   const currentDate = new Date();
+
+   const oneDayAgo = new Date();
+   oneDayAgo.setDate(currentDate.getDate() - 1);
+
+   try {
+      const result = await Event.deleteMany({
+         date: { $eq: oneDayAgo.toISOString().split('T')[0] }
+      });
+
+      console.log(`${result.deletedCount} old events deleted.`);
+   } catch (error) {
+      console.error('Error deleting old events:', error);
+   }
+};
+
+cron.schedule('0 0 * * *', () => {
+   console.log('Running a scheduled job to delete old events');
+   deleteOldEvents();
+});
