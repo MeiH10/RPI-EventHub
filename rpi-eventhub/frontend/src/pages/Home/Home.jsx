d28fb368d6c7fe0b2047060ec238e439eac1476d
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2024-11-01 16:58:40 -0400

    Did some more work on styling, but encountered issue

--- rpi-eventhub/frontend/src/pages/Home/Home.jsx
+++ rpi-eventhub/frontend/src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import ImageCarousel from "../../components/Carousel/Carousel";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { ThemeContext } from '../../context/ThemeContext';
 import { useColorScheme } from '../../hooks/useColorScheme'; 
+import Navbar from '../../components/Navbar/Navbar';
 
 const Home = () => {
   const { theme } = useContext(ThemeContext);
@@ -28,37 +29,57 @@ const Home = () => {
   };
 
   return (
-    <div className={`flex flex-col min-h-screen`} style={pageStyles} data-theme={theme}>
-      <NavBar />
-      <div className={`${HomeCSS.content} container-fluid containerFluid`}>
-        <div className="row">
-          <div className={`col-12 col-lg-7 py-sm-2 py-md-5 px-sm-0 px-md-5 text-start sm:text-center`}>
-            <div className={`${HomeCSS.anim1} title`}>
-              <h1 id="red">All RPI events,</h1>
-              <h1>in one place.</h1>
-            </div>
-            <div className={HomeCSS.grid}>
-              <div className={`${HomeCSS.searchBarWrapper} ${HomeCSS.anim2}`}>
-                  <SearchBar />
-              </div>
-              <div className={`d-block d-lg-none ${HomeCSS.carouselContainer} ${HomeCSS.anim2}`}>
-                <ImageCarousel />
-              </div>
-              <div className={`${HomeCSS.anim1} card text-start bg-transparent border-0 p-0`}>
-                <div className={`${HomeCSS.about} card-body p-0`}>
-                  <h5 className={`${HomeCSS.cardtext}`}>About the website</h5>
-                  <p className={`${HomeCSS.cardtext}`}>A comprehensive platform for RPI students and staff to effortlessly create, advertise, and explore diverse campus events, fostering a vibrant and connected university community.</p>
-                </div>
-              </div>
-            </div>
-            <hr className="text-start" /> 
-          </div>
-          <div className={`d-none d-lg-block col-lg-5 ${HomeCSS.carouselContainer} ${HomeCSS.anim2}`}>
-            <ImageCarousel />
+    // <div className={`flex flex-col min-h-screen`} style={pageStyles} data-theme={theme}>
+    //   <NavBar />
+    //   <div className={`${HomeCSS.content} container-fluid containerFluid`}>
+    //     <div className="row">
+    //       <div className={`col-12 col-lg-7 py-sm-2 py-md-5 px-sm-0 px-md-5 text-start sm:text-center`}>
+    //         <div className={`${HomeCSS.anim1} title`}>
+    //           <h1 id="red">All RPI events,</h1>
+    //           <h1>in one place.</h1>
+    //         </div>
+    //         <div className={HomeCSS.grid}>
+    //           <div className={`${HomeCSS.searchBarWrapper} ${HomeCSS.anim2}`}>
+    //               <SearchBar />
+    //           </div>
+    //           <div className={`d-block d-lg-none ${HomeCSS.carouselContainer} ${HomeCSS.anim2}`}>
+    //             <ImageCarousel />
+    //           </div>
+    //           <div className={`${HomeCSS.anim1} card text-start bg-transparent border-0 p-0`}>
+    //             <div className={`${HomeCSS.about} card-body p-0`}>
+    //               <h5 className={`${HomeCSS.cardtext}`}>About the website</h5>
+    //               <p className={`${HomeCSS.cardtext}`}>A comprehensive platform for RPI students and staff to effortlessly create, advertise, and explore diverse campus events, fostering a vibrant and connected university community.</p>
+    //             </div>
+    //           </div>
+    //         </div>
+    //         <hr className="text-start" />
+    //       </div>
+    //       <div className={`d-none d-lg-block col-lg-5 ${HomeCSS.carouselContainer} ${HomeCSS.anim2}`}>
+    //         <ImageCarousel />
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <Footer />
+    // </div>
+    <div className='min-h-screen max-w-screen flex'>
+      <Navbar />
+      <div className='flex flex-row h-full w-full justify-between mt-16 bg-blue-500'>
+        <div className='flex flex-col justify-between h-full w-full p-16 bg-red-300'>
+          <div>
+            <h1 id="red" className='text-6xl'>All RPI events,</h1>
+            <h1 className='text-6xl'>in one place.</h1>
+          </div>s
+          <SearchBar />
+          <div>
+            <h1 className='text-4xl'>About the website</h1>
+            <h1 className='text-2xl'>A comprehensive platform for RPI students and staff to effortlessly create, advertise, and explore diverse campus events, fostering a vibrant and connected university community.</h1>
           </div>
         </div>
+        <div className='flex justify-center items-center h-full w-full bg-green-400'>
+          <ImageCarousel />
+        </div>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
