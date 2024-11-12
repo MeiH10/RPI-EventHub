fc0a76feddb6ce1671ce9065446e86ab298cf32a
xiaoshaotongzhi <104879954+xiaoshaotongzhi@users.noreply.github.com>
2024-11-12 16:51:27 -0500

    update of vector similarity

--- rpi-eventhub/frontend/src/components/SearchBar/SearchBar.jsx
+++ rpi-eventhub/frontend/src/components/SearchBar/SearchBar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SearchBarCSS from './SearchBar.module.css'; // Adjust the import path if necessary
 import { useEvents } from '../../context/EventsContext'; // Adjust the import path if necessary
-
+import cosineSimilarity from 'cosine-similarity';
 const SearchBar = () => {
   const navigate = useNavigate();
   const { events, fetchEvents } = useEvents();
@@ -18,7 +18,27 @@ const SearchBar = () => {
     const filtered = events.filter(event => {
       const eventWords = event.title.toLowerCase().split(' ');
       const eventTags = event.tags.map(tag => tag.toLowerCase());
-      return searchWords.some(word => eventWords.includes(word) || eventTags.includes(word));
+      const eventDate = new Date(event.startDateTime);
+      const currentDate = new Date();
+
+      const eventText = [...eventWords, ...eventTags].join(' ');
+
+      const textToVector = (text) => {
+        const words = text.split(' ');
+        const vector = {};
+        words.forEach(word => {
+          vector[word] = (vector[word] || 0) + 1;
+        });
+        return vector;
+      };
+
+      const vectorA = textToVector(searchWords.join(' '));
+      const vectorB = textToVector(eventText);
+
+      
+      const similarity = cosineSimilarity(vectorA, vectorB);
+      
+      return similarity > 0.1 && eventDate >= currentDate;
     });
 
     const sorted = filtered.sort((a, b) => new Date(b.date) - new Date(a.date));
