1fef384697443974acb7cd1160ff4677141d935c
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2024-11-12 16:02:10 -0500

    Minor styling updates

--- rpi-eventhub/frontend/src/pages/Home/Home.jsx
+++ rpi-eventhub/frontend/src/pages/Home/Home.jsx
@@ -17,10 +17,10 @@ const Home = () => {
   return (
     <div className={`min-h-screen h-full max-w-screen flex flex-col ${isDark ? 'bg-[#120451] text-white' : 'bg-gradient-to-r from-red-400 via-yellow-200 to-blue-400 text-black'}`} data-theme={theme}>
     <Navbar />
-    <div className='flex flex-col md:flex-row h-full w-full justify-between mt-16'>
+    <div className='flex flex-col lg:flex-row h-full w-full justify-between mt-16'>
       <div className='flex flex-col justify-between h-full w-full p-16 space-y-12'>
           <div className='flex md:flex-col justify-center'>
-            <h1 id="red" className='text-4xl md:text-6xl block md:hidden'>All RPI events, {' '} in one place</h1>
+            <h1 id="red" className='text-4xl text-center md:text-6xl block md:hidden'>All RPI events, {' '} in one place</h1>
             <h1 id="red" className='text-4xl md:text-6xl hidden md:block'>All RPI events,</h1>
             <h1 id="red" className='text-4xl md:text-6xl hidden md:block'>in one place</h1>
         </div>
@@ -32,7 +32,7 @@ const Home = () => {
 
         <div>
           <h1 className='text-4xl'>About the website</h1>
-          <h1 className='text-2xl'>A comprehensive platform for RPI students and staff to effortlessly create, advertise, and explore diverse campus events, fostering a vibrant and connected university community.</h1>
+          <h1 className='text-lg md:text-2xl mt-3'>A comprehensive platform for RPI students and staff to effortlessly create, advertise, and explore diverse campus events, fostering a vibrant and connected university community.</h1>
         </div>
       </div>
 
