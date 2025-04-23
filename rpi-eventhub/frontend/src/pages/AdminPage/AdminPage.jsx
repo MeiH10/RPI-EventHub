38cf6aab2232052c90b69b37cdc239935b411d5e
Djan Carbonell <dcarbonell08@gmail.com>
2025-04-23 03:06:31 -0400

    Allow a redirect in the event of someone accidentally visiting admin link.

--- rpi-eventhub/frontend/src/pages/AdminPage/AdminPage.jsx
+++ rpi-eventhub/frontend/src/pages/AdminPage/AdminPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import styles from './AdminPage.module.css';
 import Navbar from '../../components/Navbar/Navbar';
+import { NavLink } from "react-router-dom";
 import Footer from '../../components/Footer/Footer';
 import { Skeleton } from '@mui/material';
 import { ThemeContext } from '../../context/ThemeContext';
@@ -119,7 +120,12 @@ function AdminPage() {
     ;
               } else {
                 Page = <div>
-                  <p>Unauthorized</p>
+                  <h1>Oops!</h1> 
+                  <h2>We couldn't process this link. Go back to the RPI EventHub main page, or click on the button below: </h2>
+                  <NavLink
+                  to="/all-events"
+                  className={'py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'}
+                  >Back to RPI EventHub</NavLink>
                 </div>
               }
     return (
