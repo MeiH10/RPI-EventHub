2b02fcc1e9bd3f1ea00a066f59f92d19bd7869bf
winsonlin21 <winsonlin21@gmail.com>
2025-06-06 01:03:46 -0400

    first draft of about me page
    finished the first draft of the a example page for about us

--- rpi-eventhub/frontend/src/pages/AboutUs/AboutUs.jsx
+++ rpi-eventhub/frontend/src/pages/AboutUs/AboutUs.jsx
@@ -32,10 +32,14 @@ function AboutUs() {
     <div className={`outterContainer ${isDark ? 'text-white bg-[#120451]' : 'text-black bg-gradient-to-r from-red-400 via-yellow-200 to-blue-400'}`} data-theme={theme}>
       <Navbar />
       <div className="container-fluid containerFluid">
-        <div className="row">
+        
+        <div className="row bg-white">
           <div className={`col-12 col-lg-7 px-5 ${styles.missionContainer}`}>
+            <h4 className={styles.boldTitle}>About us...</h4>
             <div className={styles.aboutUsText}>
-              <h4 className={styles.boldTitle}>Mission Statement</h4>
+              <h4 className={styles.missionStatment}>
+                Mission Statement
+              </h4>
               <div className={styles.missionText}>
                 EventHub is dedicated to connecting the students of RPI with events happening all over campus. Through this website, we hope to foster greater community, connection, and collaboration throughout the campus. Our hope is for RPI students and staff to be able to effortlessly create, advertise, and explore diverse campus events, fostering a vibrant and connected university community.
               </div>
@@ -45,22 +49,28 @@ function AboutUs() {
             <img src={RPIBridgePhoto} className={styles.bridgeImage} alt="RPI Bridge" />
           </div>
         </div>
-        <div className="row">
-          <div className="col-12 p-5 my-0 py-0">
+
+        <div className={`row bg-white ${styles.tightRowSpacing}`}>
+          <div className={`col-12 col-lg-7 px-5 ${styles.missionContainer}`}>
             <div className={styles.feedbackSection}>
               <h4 className={styles.feedbackTitle}>Feedback!</h4>
               <div>
-                We appreciate any feedback on your experience with our site. Please take a minute to fill out our 
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSclGZl30lj1o3Etb6q9oU8Q9G8zHrTUk4HC7LLNJhZfYxFiFQ/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer" className={styles.link}> feedback form</a>.
+                We appreciate any feedback on your experience with our site. Please take a minute to fill out our&nbsp;
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSclGZl30lj1o3Etb6q9oU8Q9G8zHrTUk4HC7LLNJhZfYxFiFQ/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer" className={styles.link}>feedback form</a>.
                 <br />
-                If you find any bugs or have suggestions, please submit an issue on our GitHub repository:
-                <a href="https://github.com/MeiH10/RPI-EventHub/issues" target="_blank" rel="noopener noreferrer" className={styles.link}> Report a GitHub Issue</a>.
+                If you find any bugs or have suggestions, please submit an issue on our GitHub repository:&nbsp;
+                <a href="https://github.com/MeiH10/RPI-EventHub/issues" target="_blank" rel="noopener noreferrer" className={styles.link}>Report a GitHub Issue</a>.
                 <br /><br />
                 We'd like to extend a special thanks to Corbin, <a href="https://github.com/Nibroc6" target="_blank" rel="noopener noreferrer" className={styles.link}>Nibroc6</a>, for sharing his database and providing some of the events displayed on our website.
               </div>
             </div>
           </div>
+          <div className="col-12 col-lg-5 p-5 d-flex justify-content-center align-items-center">
+            <img src={RPIBridgePhoto} className={styles.bridgeImage} alt="RPI Bridge" />
+          </div>
+          
         </div>
+
         <hr className={styles.hr} />
         <div className={styles.developers}>
           <h4 className={styles.title}>Developers</h4>
@@ -83,6 +93,7 @@ function AboutUs() {
           </div>
         </div>
         <hr className={styles.hr} />
+        
       </div>
       <Footer />
     </div>
