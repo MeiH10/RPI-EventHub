d653ceeda11768cbb79fa2bf20a16d3c4392ccc9
winsonlin21 <winsonlin21@gmail.com>
2025-12-03 20:40:26 -0500

    Finsihed fixing the footer colors
    footer should be ready for deployment

--- rpi-eventhub/frontend/src/components/Footer/Footer.jsx
+++ rpi-eventhub/frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,15 @@
 import FooterCSS from './Footer.module.css'
+import { useColorScheme } from '../../hooks/useColorScheme';
+
 
 const Footer = () => {
+  const { isDark } = useColorScheme();
+  
   return (
-    <footer className={FooterCSS.footer}>
+    <footer
+      className={FooterCSS.footer}
+      style={{ backgroundColor: isDark ? '#272727' : '#D6001C' }}
+    >
       <p className={FooterCSS.footerText}>@RPI EventHub <span className={FooterCSS.flexItem}>|</span> <a href='https://github.com/MeiH10/RPI-EventHub' ><i className="bi bi-github"></i></a> <span className={FooterCSS.flexItem}>|</span> <a href='https://github.com/MeiH10/RPI-EventHub' className={`${FooterCSS.gitHubURL}`}>An RCOS project</a></p>
     </footer>
   );
